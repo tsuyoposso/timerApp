556ac0a05a39739d08373925a946b064ae8ecb76
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-04 15:01:52 +0900

    first commit

---
+++ timerApp/timerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timerApp
+//
+//  Created by 長坂豪士 on 2019/10/04.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
